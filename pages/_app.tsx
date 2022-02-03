0c5b14d7ebb170edf2195abb2f9879c27749023a
Tanishq Singh <>
2022-02-03 09:15:48 +0530

    loader added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
 import type { AppProps } from 'next/app';
 import { EventsProvider } from '../context/event-context';
 import { DiarysProvider } from '../context/diary-context';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+NProgress.configure({
+  showSpinner: false,
+  trickleSpeed: 800
+});
+
+Router.events.on("routeChangeStart", NProgress.start);
+Router.events.on("routeChangeError", NProgress.done);
+Router.events.on("routeChangeComplete", NProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
