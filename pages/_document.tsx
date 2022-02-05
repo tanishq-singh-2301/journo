f49b1e2ac65e665caa9d76d7e2ecf22579cdf2a3
Tanishq Singh <>
2022-02-05 09:07:10 +0530

    smt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,9 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <meta name="mobile-web-app-capable" content="yes"></meta>
+                    <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
                 </ Head>
                 <body className='no-scrollbar'>
                     <Main />
